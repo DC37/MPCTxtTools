557d034044153f49411094cf79e3b6df5024d8aa
DC37 <rehdblob@gmail.com>
2013-01-29 13:36:30 -0800

    Added buttons to dialog box

---
+++ src/instruments/test.java
@@ -0,0 +1,12 @@
+package instruments;
+
+public class test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        InstrumentRepWindow.makeWindow(true);
+    }
+
+}
